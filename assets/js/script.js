f65873a223b3c0a45252b3dcbdcfc9f4543d89cd
Samuel Fuentealba <samuel.fuentealba.caceres@gmail.com>
2024-02-05 15:15:30 -0300

    primer guardado

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+/* valores ingresados por el usuario */
+let num1 = +prompt("Ingresa el primer numero");
+
+let num2 = +prompt("Ingresa el segundo numero");
+
+let resultadoSuma = num1 + num2;
+let resultadoResta = num1 - num2;
+let resultadomulti = num1 * num2;
+let resultadoDiv = num1 / num2;
+
+document.write("El resultado de la suma es: " + resultadoSuma + "<br>");
+document.write("El resultado de la resta es: " + resultadoResta + "<br>");
+document.write("El resultado de la multiplicacion es: " + resultadomulti + "<br>");
+document.write("El resultado de la division es: " + resultadoDiv + "<br>");
+
+
+
